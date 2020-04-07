86f5cad066f0c65ab3f3c3d21f62a3fa607272b1
Alexandre Gros <xelaflash@gmail.com>
2020-04-07 15:14:00 -0400

    init + files skeleton + config (eslint, yarn) ==> webpack&scss to be checked later

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+// const webpack = require('webpack');
+// const path = require('path');
+// const postcssPresetEnv = require('postcss-preset-env');
+
+// module.exports = {
+//   entry: {
+//     main: './src/javascript/index.js'
+//   },
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.m?js$/,
+//         exclude: /(node_modules)/,
+//         loader: 'babel-loader'
+//       },
+//       {
+//         test: /\.scss$/,
+//         use: ['style-loader', 'css-loader', 'sass-loader']
+//       },
+//       {
+//         test: /\.css$/,
+//         use: [
+//           'style-loader',
+//           { loader: 'css-loader', options: { importLoaders: 1 } },
+//           {
+//             loader: 'postcss-loader',
+//             options: {
+//               ident: 'postcss',
+//               plugins: () => [
+//                 postcssPresetEnv({
+//                   autoprefixer: { grid: true }
+//                 })
+//               ]
+//             }
+//           }
+//         ]
+//       },
+//       {
+//         test: /\.(jpg|jpeg|png|gif|svg|eot|ttf|woff|woff2)$/i,
+//         use: ['file-loader']
+//       }
+//     ]
+//   }
+// };

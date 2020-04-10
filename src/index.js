ae08e9ea5ad639ea32dfb19b825cc935d7a2a79c
Alexandre Gros <xelaflash@gmail.com>
2020-04-10 11:52:06 -0400

    exemple home with btn raw

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './stylesheets/style.scss';
 
 console.log('hello from index.js');
+
+const btn = document.querySelector('.enter');
+
+btn.addEventListener('click', () => {
+  alert('coucou');
+});
